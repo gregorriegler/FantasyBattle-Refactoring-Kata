8b5a2281f97db6159ce3fd9ea67054cb2f802943
Gregor Riegler <gregor.riegler@ebcont.com>
2019-07-20 10:19:45 +0200

    TEST: green

--- java/src/test/java/codingdojo/PlayerTest.java
+++ java/src/test/java/codingdojo/PlayerTest.java
@@ -25,25 +25,31 @@ public class PlayerTest {
 
     // Where	    What	                Base Damage	Damage Modifier Enemy
     // right hand	flashy sword of danger	10	        1.0
-    // SimpleEnemy has one Buff with a soakModifier of 1.0 and damage modifier of 1.0.
-    // It wears an Armor with a Damage Soak of 5.
     @Test
     public void damageCalculations() {
         // TODO 3 Problem: We do not know the domain language Buff? Soak?
         // TODO 3 Problem: We see so many dependencies, we are not sure how to specify test 1
         Inventory inventory = new Inventory(createEquipment());
-        Stats stats = new Stats(0);
-        SimpleEnemy target = new SimpleEnemy(new SimpleArmor(0), Collections.emptyList());
+
+        // TODO Problem: Requirement does not state/define strength
+        Stats stats = new Stats(10);
+        SimpleEnemy target = simpleEnemy();
         Damage damage = new Player(inventory, stats).calculateDamage(target);
         // TODO 1 Problem: need to understand all calculations inside all dependencies to understand "10"
         assertEquals(10, damage.getAmount());
+    }
+
+    // SimpleEnemy has one Buff with a soakModifier of 1.0 and damage modifier of 1.0.
+    // It wears an Armor with a Damage Soak of 5.
+    private SimpleEnemy simpleEnemy() {
+        return new SimpleEnemy(new SimpleArmor(5), Arrays.asList(new BasicBuff(1f, 1f)));
     }
 
     private Equipment createEquipment() {
         // 1. inventory needs equipment
         // 2. equipment needs items
         // TODO 2 Problem: we need to instantiate the whole tree of dependencies.
-        return new Equipment(emptyItem(), emptyItem(), emptyItem(), emptyItem(), emptyItem());
+        return new Equipment(emptyItem(), new BasicItem(10, 1), emptyItem(), emptyItem(), emptyItem());
     }
 
     private BasicItem emptyItem() {
